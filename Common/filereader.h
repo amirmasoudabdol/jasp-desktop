a3f8e314951938c020c39dde0c1cd94726d42ee0
Amir Masoud Abdol <amirmasoudabdol@icloud.com>
2021-10-20 17:40:13 +0200

    `Common` can almost be built using CMake
    There is a lot to go through here, but I am now more confident that it can be done.
    
    - Overall, the main JASP project has a `CMakeLists.txt`, and that propagates its settings to the rest of the packages, like QMake. The difference here is that CMake is much better at handling third-party libraries
    - For instance, I don't need to rely on a hard copy of the `libarchive`, `boost` or `jsoncpp`. I can just ask CMake to pull their libs and cflags and link them for me. If they support CMake themselves, then great, if not, I can use PkgConfig.
        - I can technically remove the `lib_json` and `libzip` but I haven't done that for now. This should be done on `jaspColumnEncoder`. If you want to give this a try, you probably need to fix a few header addresses on `columnencoder.h` as well since the `jsoncpp` library doesn't call itself that, instead it sits inside a `json` folder. I think I will make a parallel branch there as well, as soon as I am far somewhat happy with it.
        - The same for `libarchive`. I was quite confused, the folder is called `libzip` but the actual library is called `libarchive`. So, I couldn't decide which one it is. It's `libarchive`, `libzip` I believe is something else.
    - This build in fact fails because I still haven't done the R stuff, that's next.
    - I had to insert the `<iostream>` in a few places because we have relied on the `jsoncpp` to import it, but since we are not doing that locally, that's not valid anymore.
    - I don't like the way that I'm handling the `appinfo.cpp` but that's the CMake way. We can check if there is any better way of dealing with this.
        - CMake throws a warning because this approach confuses the Qt's CMake stuff, but that's not a big deal.
    - I am being as least destructive as I can, so I don't delete things now, but that `jsonredirect.h` should go! :cry:
    
    There is quite some more work to do here, but I think I'm getting a hang of it. I think most of those scary instructions in `.pri` files are not going to be necessary anymore. They need to be translated that we have a multi-platform build, but they are not going to be as cryptic as they are now, hopefully! :-)

--- Common/filereader.h
+++ Common/filereader.h
@@ -24,7 +24,7 @@
 #include <stdlib.h>
 #include <boost/nowide/fstream.hpp>
 
-#include "libzip/archive.h"
+#include "archive.h"
 
 /**
  * @brief The FileReader class - Reads archives or simple files.
--- Common/jsonredirect.h
+++ Common/jsonredirect.h
@@ -1,18 +1,20 @@
 #ifndef JSONREDIRECT_H
 #define JSONREDIRECT_H
 
-#ifdef JASP_LIBJSON_STATIC
-#include "lib_json/json.h"
-#else
-#ifdef LIBJSON_DIR_UP
+// Amir: I'm scared of this! Hopefully we can get rid of it and leave it to the build system.
+
+// #ifdef JASP_LIBJSON_STATIC
+// #include "lib_json/json.h"
+// #else
+// #ifdef LIBJSON_DIR_UP
 #include <json/json.h>
-#else
-#ifdef LIBJSON_DIR_CPP
-#include <jsoncpp/json/json.h>
-#else
-#include <libjson/json/json.h>
-#endif
-#endif
-#endif
+// #else
+// #ifdef LIBJSON_DIR_CPP
+// #include <jsoncpp/json/json.h>
+// #else
+// #include <lib_json/json.h>
+// #endif
+// #endif
+// #endif
 
 #endif // JSONREDIRECT_H
